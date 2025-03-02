33accad9ec64d9ca67a1e66eedc24124f4886e8d
Anish <anishm7030@gmail.com>
2025-03-01 21:35:32 -0500

    added CASTransformFunc which calculates the path of a file given its key

--- store.go
+++ store.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+// Content Addressable Storage (CAS) is a method of storing data
+// such that the key is derived from the content itself.
+func CASTransformFunc(key string) string {
+	hash := sha1.Sum([]byte(key))
+	hashString := hex.EncodeToString(hash[:])
+
+	blockSize := 5
+	sliceLen := len(hashString) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		start := i * blockSize
+		end := start + blockSize
+		paths[i] = hashString[start:end]
+	}
+
+	return strings.Join(paths, "/")
+}
 
 // PathTransformFunc is a function that transforms a key into a path.
 type PathTransformFunc func(string) string
