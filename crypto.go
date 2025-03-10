e740bf8444771f804b2a29bfaf1c7c8f9996c39a
Anish <anishm7030@gmail.com>
2025-03-10 19:05:55 -0400

    added copyDecrypt to read and store files from peers

--- crypto.go
+++ crypto.go
@@ -7,6 +7,48 @@ import (
 	"io"
 )
 
+func newEncryptionKey() []byte {
+	keyBuf := make([]byte, 32)
+	io.ReadFull(rand.Reader, keyBuf)
+	return keyBuf
+}
+
+func copyDecrypt(key []byte, src io.Reader, dst io.Writer) (int64, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return 0, err
+	}
+
+	// Read the iv from the given io.Reader
+	iv := make([]byte, block.BlockSize())
+	if _, err := src.Read(iv); err != nil {
+		return 0, err
+	}
+
+	var (
+		buf    = make([]byte, 32*1024)
+		stream = cipher.NewCTR(block, iv)
+	)
+
+	for {
+		n, err := src.Read(buf)
+		if n > 0 {
+			stream.XORKeyStream(buf, buf[:n])
+			if _, err := dst.Write(buf[:n]); err != nil {
+				return 0, err
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return 0, nil
+}
+
 func copyEncrypt(key []byte, src io.Reader, dst io.Writer) (int64, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
--- crypto_test.go
+++ crypto_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCopyEncryptDecrypt(t *testing.T) {
+	payload := "anish anish anish"
+	src := bytes.NewReader([]byte(payload))
+	dst := new(bytes.Buffer)
+	key := newEncryptionKey()
+	_, err := copyEncrypt(key, src, dst)
+	if err != nil {
+		t.Error(err)
+	}
+
+	out := new(bytes.Buffer)
+	if _, err := copyDecrypt(key, dst, out); err != nil {
+		t.Error(err)
+	}
+
+	if out.String() != payload {
+		t.Errorf("Expected %s, got %s", payload, out.String())
+	}
+}
